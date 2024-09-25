365e430a73bce6401524f7cbf5dac4da825f9512
Mudy Mud <bastien.masselon@laposte.net>
2024-09-25 15:49:49 +0200

    Challenge : composant Article (card)

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './article.component.scss'
 })
 export class ArticleComponent {
-
+  title: string = 'Ce titre est fabuleux';
+  content: string = 'Il y a des choses qui ne se font pas, telles que de boire du Dom Pérignon 55 à une température au-dessus de trois degrés et écouter les Beatles sans boules Quiès.'
 }
